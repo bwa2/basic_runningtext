e136fb58b53045178098d63d7072c47af4ac1f75
intern22rkb <intern@riset.ai>
2023-07-24 14:11:32 +0700

    test

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 print("hello mau coba git")
 print("coba pake gitbash")
-print("coba lagi")
+# git remote set-url origin https://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@github.com/bwa2/basic_runningtext
+
+
