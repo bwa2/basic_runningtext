ce3b2e2c64382e0b4557228602b5a187d5929a37
radityabw-stei <99741179+radityabw-stei@users.noreply.github.com>
2023-07-26 11:20:46 +0700

    calculator time HH:MM:SS

---
+++ calculatortime.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+
+sec = 3765
+print('Time in Seconds:', sec)
+
+td = timedelta(seconds=sec)
+print('Time in hh:mm:ss:', td)
+
+str_sec = str(timedelta(seconds=sec))
+# Use the below code if you want it in a string
+print(str_sec)
