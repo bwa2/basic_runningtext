53acac38e94fb017ded685a95e2c4e3ebc89fc3b
radityabw-stei <raditya.wardhanabagus@gmail.com>
2023-07-26 16:03:19 +0700

    nambah cetak json

--- running-text-2.py
+++ running-text-2.py
@@ -224,7 +224,7 @@ for i in range(jml_berita):
     if(i != jml_berita-1):
         input_json.append({})
 
-with open("cobatime.json", "w") as f:
+with open("cobatime-2.json", "w") as f:
     json.dump(input_json,f, indent=3)
 f.close()
 
