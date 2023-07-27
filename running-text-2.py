d52ad7a8e5e715c6d283b248678fe6cc9abe4160
radityabw-stei <raditya.wardhanabagus@gmail.com>
2023-07-27 15:24:22 +0700

    debugging flag karena counter kurang dari

--- running-text-2.py
+++ running-text-2.py
@@ -153,8 +153,8 @@ while cap.isOpened():
             print("time:",sec)
             print("counter starttime:",counter)
             
-            # if sec>1250:
-            #     break
+            if sec>1570:
+                break
 
             if (flag_timer==True) or (flag_timer_break==True) or (flag_timer_prebreak==True):
                 arr_start.append(sec)
