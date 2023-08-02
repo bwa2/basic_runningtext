bea215dd60dc137f2b8ea9fbb92e90f4f1920f21
Kadhan Dalilurahman <82268450+bwa2@users.noreply.github.com>
2023-08-02 15:35:06 +0700

    Update and rename main.py to contoh-penggunaan-ml.py

--- contoh-penggunaan-ml.py
+++ contoh-penggunaan-ml.py
@@ -7,19 +7,10 @@ from tensorflow.keras.preprocessing import image
 detik =0
 frame_count = 0
 iter = 0
-detik_iklan = []
-frame_3 = []
-
-logdir = './frames'
-if not os.path.exists(logdir):
-    os.makedirs(logdir)
-
 
 cap = cv2.VideoCapture('cek-iklan2-inews.mp4')
 
-# video properti
-fps = int(round(cap.get(cv2.CAP_PROP_FPS)))
-
+# mengimport file model yang ada di gdrive https://drive.google.com/file/d/1wapNSst2IlPui8HIki7PWYkfGWW7Nmc5/view?usp=sharing
 model = tf.keras.models.load_model('modelv2.h5')
 
 while cap.isOpened():
@@ -27,35 +18,24 @@ while cap.isOpened():
     if ret:
         if (iter%fps) == 0:
             detik += 1 
-            
+
+            # menginput frame video ke dalam format yang dapat diterima
             frame_2 = cv2.resize(frame, (300, 300))
             x = np.expand_dims(frame_2,0)
 
-            if detik == 3:
-                frame_3 = x
-            
-
+            # model melakukan prediksi dari frame yang telah diformat
             classes = model.predict(x)
             print(classes)
 
-            print("detik: ", detik)
+            # mengekstrak hasil prediksi
             if classes[0][0] == 1:
                 print("IKLAN")
                 detik_iklan.append(detik)
             elif classes[0][1] == 1:
                 print("INEWS")
-            
-            frame_count += 1
-            cv2.imwrite(f'./frames/frame_{frame_count}.jpg', frame_2)
+                
         iter += 1
     else:
         break
-
-print(detik_iklan)
-
-#f = open("liat2.txt", "a")
-
-#f.write(f"{frame_3}")
-#f.close()
 
 cap.release()
