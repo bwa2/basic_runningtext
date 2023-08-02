d053ac84430a0b959c3e8dfc2ba7e8cd50d1fe40
radityabw-stei <raditya.wardhanabagus@gmail.com>
2023-08-02 14:23:54 +0700

    cnbc updated

--- running-text-cnbc.py
+++ running-text-cnbc.py
@@ -18,7 +18,7 @@ height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # preprocess
 height_process_top = round((23.5 / 27) * height)
-height_process_bottom = round((26.5 / 27) * height) #26.5
+height_process_bottom = round((25 / 27) * height) #26.5
 width_process_left = round((1/7.6) * width)
 width_process_right = round((6.2/7.6) * width)
 
@@ -28,9 +28,9 @@ sec = 0
 
 flag_mulai = True
 
-news = ["#*"]
+
 temp_result = ""
-temp_news = ["#*"]
+temp_news = ["#&"]
 
 reader = easyocr.Reader(['id'], gpu=True)
 
@@ -44,13 +44,26 @@ while cap.isOpened():
             frame_2 = frame_2[height_process_top:height_process_bottom,
                               width_process_left:width_process_right]
 
+            # preprocessing tambahan
+            # frame_2 = cv2.GaussianBlur(frame_2,(5,5),0)
+            # #img = cv2.medianBlur(img, 3)
+            # a, frame_2 = cv2.threshold(frame_2, 120, 255, cv2.THRESH_BINARY)
+            # frame_2 = cv2.fastNlMeansDenoisingColored(frame_2, None, 10, 10, 7, 15)    
+
             # ocr
-            result = reader.readtext(frame_2,mag_ratio=1.4,blocklist='.')
+            result = reader.readtext(frame_2,mag_ratio=1.3)
             print(result)
             
-            temp_result += " " + result[2][1]
+            temp_result = result[-2][1]
+            
+            if sm(None, "".join(temp_news[-1]), "".join(temp_result)).ratio() < 0.85:
+                temp_news.append(temp_result)
 
-            print("temp_news:",temp_result)
+            if temp_news[-1][-1]==")":
+                temp_news[-1] += "&"
+
+
+            print("temp_news:",temp_news)
             arr_distance, frame_2 = bounding_box(result,frame_2)
             print("arr distance: ",arr_distance)
             print("----------------")
@@ -58,15 +71,20 @@ while cap.isOpened():
 
             frame_count += 1
             # if frame_count>3600:
-            # cv2.imwrite(f'frame_{frame_count}.jpg', frame_2)
-
-
+            cv2.imwrite(f'frame_{frame_count}.jpg', frame_2)
+            sec+=1
+            if sec>15:
+                break
         iter += 1
     else:
         break
 
-
-
-
 cap.release()
 # cv2.destroyAllWindows()
+
+# buat misahin per berita
+news = " ".join(temp_news)
+news = news.split("&")
+if len(news[-1])==0:
+    news = news[:-1]
+print(news)
