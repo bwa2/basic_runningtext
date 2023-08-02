2a0b14b58e15628f97b4bf42565070329868fb30
radityabw-stei <raditya.wardhanabagus@gmail.com>
2023-08-02 09:50:09 +0700

    edit config inews

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # preprocess
 def inews (width, height) :
-    height_process_top = round((24.5 / 27) * height)
+    height_process_top = round((24.8 / 27) * height)
     height_process_bottom = round((26 / 27) * height)
-    width_process_left = round((1.25999/7.6) * width)
+    width_process_left = round((1.5/7.6) * width)
     width_process_right = round((7.6/7.6) * width)
 
     return height_process_top, height_process_bottom,  width_process_left, width_process_right
