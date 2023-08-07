ae50d20669655ebd3c7720ce04a846820f4abd6e
radityabw-stei <raditya.wardhanabagus@gmail.com>
2023-08-07 11:14:07 +0700

    output json

--- running-text-cnbc.py
+++ running-text-cnbc.py
@@ -9,7 +9,7 @@ import json
 
 
 
-cap = cv2.VideoCapture("../../cnbc-sejam/cnbc-sejam-duel2021.mp4")
+cap = cv2.VideoCapture("../../cnbc-sejam/cnbc-sejam-terjebak.mp4")
 #cap = cv2.VideoCapture("Videos/3mnt-cnbc.mp4")
 
 # get video property
