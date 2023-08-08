f0b17a93552ba8184963d9de57f699105592658c
radityabw-stei <raditya.wardhanabagus@gmail.com>
2023-08-08 14:42:57 +0700

    debugging iklan

--- running-text-cnbc.py
+++ running-text-cnbc.py
@@ -115,7 +115,6 @@ while cap.isOpened():
                     
 
                     # main processing running text bagian atas
-                    if len(result==0):
                     temp_result_atas = result[-2][1]
                     # if temp_news[-1][0].isdigit() and (not(temp_result_atas[0].isdigit())):
                     #     if temp_news[-1][-1]==")":
